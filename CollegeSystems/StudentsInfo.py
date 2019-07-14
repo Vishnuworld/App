eeb3293f737b4277de83281408103ca2c369b987
VishnuWorld <52811937+Vishnuworld@users.noreply.github.com>
2019-07-14 12:39:31 +0530

    Update StudentsInfo.py

--- CollegeSystems/StudentsInfo.py
+++ CollegeSystems/StudentsInfo.py
@@ -7,7 +7,7 @@ class Student:
         self.StudAge=age
         self.StudGender=gender
         self.StudBloodG=bloodg
-        self.StudentStatus=status11
+        self.StudentStatus=status111
 
 
 
