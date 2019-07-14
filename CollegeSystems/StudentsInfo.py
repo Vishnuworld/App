a5f66450fb6e40bb916fef27a806bf3b06983e3b
vishnu <vbhandari9561@gmail.com>
2019-07-14 12:42:02 +0530

    aa

--- CollegeSystems/StudentsInfo.py
+++ CollegeSystems/StudentsInfo.py
@@ -7,7 +7,7 @@ class Student:
         self.StudAge=age
         self.StudGender=gender
         self.StudBloodG=bloodg
-        self.StudentStatus=status
+        self.StudentStatus=status123
 
 
 
